9772aef3fca5998ddd86a90365bc1aa227a3bfca
GBTC406005UR <GBTC406005UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-11-05 15:23:01 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBoot401bApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot401bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot401bApplication.class, args);
+	}
+}
